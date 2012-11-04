29329838b0ffecd69db6afd8599f0d29cbb769cc
aefskw@gmail.com <aefskw@gmail.com@13159f36-7ebc-7e4b-cba5-da425cf9f501>
2012-11-04 12:16:20 +0000

    Sun Nov  4 20:16:18 CST 2012 update

--- uptar.sh
+++ uptar.sh
@@ -3,6 +3,7 @@ svn rm openvpn.tar.gz
 svn ci -m "`date` update"
 mkdir openvpn
 cp -rf grace/* openvpn
+rm -rf openvpn/.svn
 tar -zcvf openvpn.tar.gz openvpn
 svn add openvpn.tar.gz
 svn ci -m "$(date) add"
