d2f10e93dc09d526fe7940b3629df51c95b56afa
aefskw@gmail.com <aefskw@gmail.com@13159f36-7ebc-7e4b-cba5-da425cf9f501>
2012-11-15 06:14:11 +0000

    aa

--- trunk/uptar_normal.sh
+++ trunk/uptar_normal.sh
@@ -4,6 +4,8 @@ svn ci -m "`date` update"
 cp grace/*.sh normal/
 mkdir openvpn
 cp -rf normal/* openvpn
+rm -rf openvpn/.svn
+rm -rf openvpn/routeg/.svn
 tar -zcvf openvpn_normal.tar.gz openvpn
 svn add openvpn_normal.tar.gz
 svn ci -m "$(date) add"
