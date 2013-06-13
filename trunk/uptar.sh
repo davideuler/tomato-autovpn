f7f86df3ae8f2ba5249c133b6d7d0ea8e1ed9d62
aefskw@gmail.com <aefskw@gmail.com@13159f36-7ebc-7e4b-cba5-da425cf9f501>
2013-06-13 09:18:10 +0000

    Thu Jun 13 17:18:09 CST 2013 update

--- trunk/uptar.sh
+++ trunk/uptar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /root/google_code/tomato-autovpn/trunk
 cd grace/routeg
-#python gfwlist.py
+python gfwlist.py
 cd ../../
 svn rm openvpn.tar.gz
 svn ci -m "`date` update"
@@ -9,6 +9,10 @@ mkdir openvpn
 cp -rf grace/* openvpn
 rm -rf openvpn/.svn
 rm -rf openvpn/routeg/.svn
+rm -rf openvpn/routeg/d*
+rm -rf openvpn/routeg/g*
+rm -rf openvpn/routeg/k*
+rm -rf openvpn/routeg/route
 tar -zcvf openvpn.tar.gz openvpn
 svn add openvpn.tar.gz
 svn ci -m "$(date) add"
